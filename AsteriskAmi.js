2cf62d2337c55bc7dc6d10a167f6f60bbf0c39de
arefazizii <AZIZIAREF84@GMAIL.COM>
2023-04-27 14:54:25 +0430

    fixed insert eror and handle voip logs in crm front

--- AsteriskAmi.js
+++ AsteriskAmi.js
@@ -2,7 +2,8 @@ const AmiClient = require('asterisk-ami-client')
 const main = require('./main');
 const axios = require('axios');
 const sleep = require('sleep-promise');
-const { WebSocketServer } = require('ws')
+const { WebSocketServer } = require('ws');
+const { string } = require('joi');
 const sockserver = new WebSocketServer({ port: 8081 })
 const voip = new AmiClient()
 
@@ -12,16 +13,15 @@ const voip = new AmiClient()
 
 
 voip.connect('admin', '0938204386', { host: '172.16.10.1', port: 5038 })
-  // voip.disconnect()
   .then(() => {
     voip
-      .on('connect', () => console.log('connect')) // show connection logs in terminal
-      .on('event', event => handleEvent(event)
-      )
-      .on('response', response => console.log(response)) // show response logs in terminal
-      .on('disconnect', () => console.log('disconnect')) // show disconnection logs in terminal
-      .on('reconnection', () => console.log('reconnection')) // show reconnection logs in terminal
-      .on('internalError', error => console.log(error)) // show AMI error logs in terminal
+      .on('connect', () => console.log('connect'))
+      .on('event', event =>
+        handleEvent(event))
+      .on('response', response => console.log(response))
+      .on('disconnect', () => console.log('disconnect'))
+      .on('reconnection', () => console.log('reconnection'))
+      .on('internalError', error => console.log(error))
   })
   .catch(error => console.log(error))
 
@@ -29,15 +29,13 @@ voip.connect('admin', '0938204386', { host: '172.16.10.1', port: 5038 })
 
 function handleEvent(event) {
   sendLog(event)
-
+  sleep(3000)
   if (event.Event === 'Cdr') {
-    if (event.Disposition !== 'CONGESTION') {
+    if (event.Destination !== '801' && event.Destination !== '802' && event.Destination !== '803' && event.Destination !== '804' && event.Destination !== '806') {
       insVoipLog(event)
     }
   }
 }
-
-
 
 
 function insVoipLog(event) {
@@ -45,6 +43,7 @@ function insVoipLog(event) {
 
     var caller = event.CallerID.replace("<", "/ ").replace(">", "").replace("\"", "").replace("\"", "");
     event.CallerID = caller
+    sleep(2000)
     axios.post('http://localhost:8080/tws/crm/voiplogs', {
       Event: event.Event,
       Privilege: event.Privilege,
@@ -77,27 +76,93 @@ function insVoipLog(event) {
 }
 
 function sendLog(event) {
-  if (main.internalNumber == null) { exports.logMessage = null}
-  else {
-    if ( event.ConnectedLineNum == main.internalNumber || event.CallerIDNum == main.internalNumber) {
-      console.log(event)
-        var logMessage = event.ConnectedLineName + " با شماره  " + event.ConnectedLineNum + " در حال تماس با  " + event.CallerIDNum + "/" + event.CallerIDName + " است"
+  // if (main.internalNumber == null) { exports.logMessage = null}
+  // else {DeviceStateChange
+  // console.log(event)
+  if (event.Event !== "RTCPSent" && event.Event !== "VarSet" && event.Event !== "RTCPReceived" && event.Event !== "LocalBridge" && event.Event !== "DeviceStateChange" && event.Event !== "QueueMemberStatus" && event.Event !== "ExtensionStatus" && event.Event !== "PeerStatus" && event.Event !== "BridgeCreate") {
+
+    // console.log(event)
+    if (event.Event === 'NewConnectedLine' || event.Event === 'NewCallerid' || event.Event === 'Cdr') {
+      // console.log(event.Event)
+
+      if (event.ConnectedLineNum !== "<unknown>" && event.CallerIDNum !== "<unknown>" && event.ConnectedLineName !== "<unknown>" && event.CallerIDName !== "<unknown>") {
+        // console.log(event.CallerID)
+        // console.log(event.Destination)
+          CallerIDNum = event.CallerIDNum,
+          CallerIDName = event.CallerIDName,
+          ConnectedLineName = event.ConnectedLineName,
+          ConnectedLineNum = event.ConnectedLineNum,
+          CallState = event.Event
+        if (event.Event === 'Cdr') {
+          // console.log("its cdr")
+          CallerID = event.CallerID.replace("<", "/ ").replace(">", "").replace("\"", "").replace("\"", "").split("/")[1].trim()
+          Destination = event.Destination
+          var logMessage = {
+            CallerIDNum,
+            CallerIDName,
+            ConnectedLineName,
+            ConnectedLineNum,
+            CallerID,
+            Destination,
+            CallState
+          };
+        } else {
+          // console.log("its not cdr")
+          var logMessage = {
+            CallerIDNum,
+            CallerIDName,
+            ConnectedLineName,
+            ConnectedLineNum,
+            CallState
+          };
+        }
+        // var logMessage = 
+        //   {
+        //   0: CallerIDNum,
+        //   1:CallerIDName,
+        //   2:ConnectedLineName, 
+        //   3:ConnectedLineNum
+        //   }
+        // var logMessage = [
+        //    CallerIDNum,
+        //    CallerIDName,
+        //    ConnectedLineName,
+        //    ConnectedLineNum
+        // ];
+        // console.log(CallerID)
+        // console.log(CallerID[0])
+        // console.log(CallerID[1])
+        // var logMessage = {
+        //   CallerIDNum,
+        //   CallerIDName,
+        //   ConnectedLineName,
+        //   ConnectedLineNum,
+        //   CallerID,
+        //   Destination,
+        //   CallState
+        // };
         module.exports.logMessage = logMessage;
-      if (event.Event === "Hangup") {
-        console.log('hanged up')
-        exports.logMessage = null
+        // if (event.Event === "Hangup") {
+        //   console.log('hanged up')
+        //   exports.logMessage = null
+        // }
       }
     }
   }
-
+  // }
+  return logMessage
 }
 
-sockserver.on('connection', ws => {
-  // console.log('New client connected!')
-  // console.log(exports.logMessage)
 
-  ws.send(exports.logMessage)
-  // ws.on('close', () => console.log('Client has disconnected!'))
+sockserver.on('connection', ws => {
+  console.log('New client connected!')
+  // console.log(exports.logMessage)
+  // var test = sendLog()
+  //  console.log(test)
+  // while(true){
+  // sleep(2000)
+  ws.on('close', () => console.log('Client has disconnected!'))
+  ws.send(JSON.stringify(exports.logMessage))
   ws.on('message', data => {
     sockserver.clients.forEach(client => {
       console.log(`distributing message: ${data}`)
